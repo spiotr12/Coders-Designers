b58a187d2ed329cc7df83d2a0da05f27b2be1438
adrian <1204969@rgu.ac.uk>
2016-03-14 12:30:42 +0000

    Fields added to the user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313235323) do
+ActiveRecord::Schema.define(version: 20160314002553) do
 
   create_table "bios", force: :cascade do |t|
     t.string   "name"
@@ -72,6 +72,14 @@ ActiveRecord::Schema.define(version: 20160313235323) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "position"
+    t.text     "bio"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "website"
+    t.string   "linkedin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
